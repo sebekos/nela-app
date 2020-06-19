8a161881471b5118e0615e6638681ec8868ff392
sebekos <sebkpl@gmail.com>
2020-06-18 20:29:15 -0500

    Alpha styling

--- client/src/components/family/AlphaSearch.js
+++ client/src/components/family/AlphaSearch.js
@@ -2,15 +2,26 @@ import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import { uuid } from "uuidv4";
+import styled from "styled-components";
+
+const AlphaContainer = styled.div`
+    display: flex;
+    width: max-content;
+    margin: 2rem auto;
+`;
 
 const AlphaKeys = ({ data }) => {
-    return data.map((char) => {
-        return (
-            <p key={uuid()}>
-                {char.alpha} ({char.num})
-            </p>
-        );
-    });
+    return (
+        <AlphaContainer>
+            {data.map((char) => {
+                return (
+                    <p key={uuid()}>
+                        {char.alpha} ({char.num})
+                    </p>
+                );
+            })}
+        </AlphaContainer>
+    );
 };
 
 const AlphaSearch = () => {
